745baf27f6980b8e2efda03e8f43b3e7b1244a61
foxx-7 <unachukwu246@gmail.com>
2025-03-26 00:14:39 +0100

    Add files via upload

---
+++ FileServer.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+
+class FileServer{
+  public static void main(String[] args){
+    ServerSocket serverSocket = null;
+    try{  
+      serverSocket = new ServerSocket(8000);
+        System.out.println("server listening on port 8000!");
+      while(true){
+      Socket  clientSocket = serverSocket.accept();
+       new ClientHandler(clientSocket).handle();
+      }
+    }catch(IOException e){
+      System.out.println("error : " + e.getMessage());
+    }finally{
+      try{
+        if(!serverSocket.isClosed()){
+          serverSocket.close();
+        }
+      }catch(IOException e){
+          System.out.println("error : " + e.getMessage());
+      }
+    }
+  }
+}
